111f65bdc153765f07d8db2022d1afc7c8848da4
yakshit0511 <23it047@charusat.edu.in>
2026-07-07 00:47:45 +0530

    chore: Update sitemap & robots domain to production Vercel frontend URL

--- client/scripts/generateSitemap.js
+++ client/scripts/generateSitemap.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const domain = "https://adityabuilders.in";
+const domain = "https://aditya-builders-dz-infotech.vercel.app";
 const staticPages = [
   "",
   "/about",
